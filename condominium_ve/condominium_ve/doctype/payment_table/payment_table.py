6e39bd24fc04413173c22df7c5aac4e56472ba95
Armando Rojas <armando.develop@gmail.com>
2022-11-25 10:02:51 -0400

    payment table

--- condominium_ve/condominium_ve/doctype/payment_table/payment_table.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Armando Rojas and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class PaymentTable(Document):
-	pass
