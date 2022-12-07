0e93c065060236484e9ebbb2b4fcc9aa4e374e63
Armando Rojas <armando.develop@gmail.com>
2022-12-07 11:30:24 -0400

    trabajo programado

--- condominium_ve/hooks.py
+++ condominium_ve/hooks.py
@@ -107,7 +107,7 @@ app_license = "MIT"
 # ---------------
 scheduler_events = {
     "cron": {
-        "*/15 * * * * *": [
+        "*/1 * * * * *": [
             "condominium_ve.task.validate_payment"
         ]
     }
