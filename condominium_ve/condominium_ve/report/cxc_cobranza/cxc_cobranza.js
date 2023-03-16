2bf6ed7c08b1a440593b5f392fcfeaf169184f6f
Akkalame Ereut <akkalameo.o@gmail.com>
2023-03-16 11:31:09 -0400

    mejora en los filtros del reporte

--- condominium_ve/condominium_ve/report/cxc_cobranza/cxc_cobranza.js
+++ condominium_ve/condominium_ve/report/cxc_cobranza/cxc_cobranza.js
@@ -6,27 +6,228 @@ frappe.query_reports["CxC Cobranza"] = {
 	"parent_field": "name",
 	"initial_depth": 1,			//The level to which the initial rendering will expand to
 	
-	onload: function(report) {
-		// dropdown for links to other financial statements
-		//erpnext.financial_statements.filters = get_filters()
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname": "report_date",
+			"label": __("Posting Date"),
+			"fieldtype": "Date"
+			"default": frappe.datetime.get_today()
+		},
+		{
+			"fieldname": "finance_book",
+			"label": __("Finance Book"),
+			"fieldtype": "Link",
+			"options": "Finance Book"
+		},
+		{
+			"fieldname": "cost_center",
+			"label": __("Cost Center"),
+			"fieldtype": "Link",
+			"options": "Cost Center",
+			get_query: () => {
+				var company = frappe.query_report.get_filter_value('company');
+				return {
+					filters: {
+						'company': company
+					}
+				};
+			}
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer",
+			on_change: () => {
+				var customer = frappe.query_report.get_filter_value('customer');
+				var company = frappe.query_report.get_filter_value('company');
+				if (customer) {
+					frappe.db.get_value('Customer', customer, ["tax_id", "customer_name", "payment_terms"], function(value) {
+						frappe.query_report.set_filter_value('tax_id', value["tax_id"]);
+						frappe.query_report.set_filter_value('customer_name', value["customer_name"]);
+						frappe.query_report.set_filter_value('payment_terms', value["payment_terms"]);
+					});
 
-		//let fiscal_year = frappe.defaults.get_user_default("fiscal_year")
-		/*
-		frappe.model.with_doc("Fiscal Year", fiscal_year, function(r) {
-			var fy = frappe.model.get_doc("Fiscal Year", fiscal_year);
-			frappe.query_report.set_filter_value({
-				period_start_date: fy.year_start_date,
-				period_end_date: fy.year_end_date
-			});
-		});
-		*/
+					frappe.db.get_value('Customer Credit Limit', {'parent': customer, 'company': company},
+						["credit_limit"], function(value) {
+						if (value) {
+							frappe.query_report.set_filter_value('credit_limit', value["credit_limit"]);
+						}
+					}, "Customer");
+				} else {
+					frappe.query_report.set_filter_value('tax_id', "");
+					frappe.query_report.set_filter_value('customer_name', "");
+					frappe.query_report.set_filter_value('credit_limit', "");
+					frappe.query_report.set_filter_value('payment_terms', "");
+				}
+			}
+		},
+		{
+			"fieldname": "party_account",
+			"label": __("Receivable Account"),
+			"fieldtype": "Link",
+			"options": "Account",
+			get_query: () => {
+				var company = frappe.query_report.get_filter_value('company');
+				return {
+					filters: {
+						'company': company,
+						'account_type': 'Receivable',
+						'is_group': 0
+					}
+				};
+			}
+		},
+		{
+			"fieldname": "ageing_based_on",
+			"label": __("Ageing Based On"),
+			"fieldtype": "Select",
+			"options": 'Posting Date\nDue Date',
+			"default": "Due Date"
+		},
+		{
+			"fieldname": "range1",
+			"label": __("Ageing Range 1"),
+			"fieldtype": "Int",
+			"default": "30",
+			"reqd": 1
+		},
+		{
+			"fieldname": "range2",
+			"label": __("Ageing Range 2"),
+			"fieldtype": "Int",
+			"default": "60",
+			"reqd": 1
+		},
+		{
+			"fieldname": "range3",
+			"label": __("Ageing Range 3"),
+			"fieldtype": "Int",
+			"default": "90",
+			"reqd": 1
+		},
+		{
+			"fieldname": "range4",
+			"label": __("Ageing Range 4"),
+			"fieldtype": "Int",
+			"default": "120",
+			"reqd": 1
+		},
+		{
+			"fieldname": "customer_group",
+			"label": __("Customer Group"),
+			"fieldtype": "Link",
+			"options": "Customer Group"
+		},
+		{
+			"fieldname": "payment_terms_template",
+			"label": __("Payment Terms Template"),
+			"fieldtype": "Link",
+			"options": "Payment Terms Template"
+		},
+		{
+			"fieldname": "sales_partner",
+			"label": __("Sales Partner"),
+			"fieldtype": "Link",
+			"options": "Sales Partner"
+		},
+		{
+			"fieldname": "sales_person",
+			"label": __("Sales Person"),
+			"fieldtype": "Link",
+			"options": "Sales Person"
+		},
+		{
+			"fieldname": "territory",
+			"label": __("Territory"),
+			"fieldtype": "Link",
+			"options": "Territory"
+		},
+		{
+			"fieldname": "group_by_party",
+			"label": __("Group By Customer"),
+			"fieldtype": "Check"
+		},
+		{
+			"fieldname": "based_on_payment_terms",
+			"label": __("Based On Payment Terms"),
+			"fieldtype": "Check",
+		},
+		{
+			"fieldname": "show_future_payments",
+			"label": __("Show Future Payments"),
+			"fieldtype": "Check",
+		},
+		{
+			"fieldname": "show_delivery_notes",
+			"label": __("Show Linked Delivery Notes"),
+			"fieldtype": "Check",
+		},
+		{
+			"fieldname": "show_sales_person",
+			"label": __("Show Sales Person"),
+			"fieldtype": "Check",
+		},
+		{
+			"fieldname": "show_remarks",
+			"label": __("Show Remarks"),
+			"fieldtype": "Check",
+		},
+		{
+			"fieldname": "tax_id",
+			"label": __("Tax Id"),
+			"fieldtype": "Data",
+			"hidden": 1
+		},
+		{
+			"fieldname": "customer_name",
+			"label": __("Customer Name"),
+			"fieldtype": "Data",
+			"hidden": 1
+		},
+		{
+			"fieldname": "payment_terms",
+			"label": __("Payment Tems"),
+			"fieldtype": "Data",
+			"hidden": 1
+		},
+		{
+			"fieldname": "credit_limit",
+			"label": __("Credit Limit"),
+			"fieldtype": "Currency",
+			"hidden": 1
+		}
+	],
+	/*
+	"formatter": function(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (data && data.bold) {
+			value = value.bold();
+
+		}
+		return value;
+	},*/
+
+	onload: function(report) {
+		//console.log(report.get_values());
+		//report.filters.report_date = frappe.datetime.get_today()
+		//console.log(frappe.datetime.get_today());
+		
 		//console.log(report.get_values());
 		report.page.add_inner_button(__('Enviar Correos'), () => {
 			//console.log(report.get_values());
 		  let filtros = report.get_values();
-		  console.log(filtros);
+		  
 		  filtros['docstatus'] = 1;
-
+		  
           frappe.call({
             method:
               "condominium_ve.condominium_ve.report.cxc_cobranza.cxc_cobranza.send_email",
@@ -48,55 +249,10 @@ frappe.query_reports["CxC Cobranza"] = {
 
       });
 		
-	},
+	}
 	
 
-	"filters": [
-		{
-			fieldname: "company",
-			label: "Empresa",
-			fieldtype: "Link",
-			reqd: 1,
-			options: 'Company'
-			//default: (frappe.user.has_role("Administrator") || frappe.user.has_role("System Manager")) ? "" : frappe.user.name,
-			
-		},
-		{
-			fieldname: "report_date",
-			label: "Fecha de contabilizacion",
-			fieldtype: "Date",
-			reqd: 1
-			//default: (frappe.user.has_role("Administrator") || frappe.user.has_role("System Manager")) ? "" : frappe.user.name,
-			
-		},
-		{
-			fieldname: "customer",
-			label: "Cliente",
-			fieldtype: "Link",
-			options: 'Customer'
-			//default: (frappe.user.has_role("Administrator") || frappe.user.has_role("System Manager")) ? "" : frappe.user.name,
-			
-		},
-		{
-			fieldname: "territory",
-			label: "Sector",
-			fieldtype: "Link",
-			options: 'Territory'
-			//default: (frappe.user.has_role("Administrator") || frappe.user.has_role("System Manager")) ? "" : frappe.user.name,
-			
-		},
-		{
-			fieldname: "group_by_party",
-			label: "Agrupar por Cliente",
-			fieldtype: "Check"
-			//default: (frappe.user.has_role("Administrator") || frappe.user.has_role("System Manager")) ? "" : frappe.user.name,
-			
-		}
-
-		
-	]
-};
-
-function EnviarCorreos(filtros){
-	console.log(filtros);
+	
 }
+
+//erpnext.utils.add_dimensions('Accounts Receivable', 9);
