5df5c6d3ed8acd4bbc9f45ce48698650b998a5dd
Akkalame Ereut <akkalameo.o@gmail.com>
2023-05-18 11:58:37 -0400

    unidades activas y pertenecientes al condominio se actualizan de manera automatica

--- condominium_ve/condominium_ve/doctype/housing/housing.py
+++ condominium_ve/condominium_ve/doctype/housing/housing.py
@@ -9,7 +9,34 @@ from frappe.model.document import Document
 from frappe.utils.response import build_response
 
 class Housing(Document):
-	pass
+	def validate(self):
+		
+		condominium_doc = frappe.get_doc('Condominium', self.condominium)
+
+		if not self.inserted:
+			# actualiza el campo inserted en el documento de housing
+			
+			self.inserted = 1
+			#self.save()
+			condominium_doc.n_houses += 1
+			if self.active:
+				condominium_doc.n_houses_active += 1
+			
+		else:
+			if self.active:
+				condominium_doc.n_houses_active += 1
+			else:
+				condominium_doc.n_houses_active -= 1
+		condominium_doc.save()
+	
+	def on_trash(self):
+		is_house_active = frappe.db.get_value('Housing', self.name, 'active')
+		condominium_doc = frappe.get_doc('Condominium', self.condominium)
+		condominium_doc.n_houses -= 1
+		if is_house_active:
+			condominium_doc.n_houses_active -= 1
+		
+		condominium_doc.save()
 
 def get_random_string(length):
     letters = string.ascii_uppercase
@@ -29,21 +56,26 @@ def generate_code():
 	frappe.local.response.update({"data": {  'code': code  } })
 
 	return build_response("json")
-
+"""
 @frappe.whitelist()
-def update_condominium_houses(condominium, active, inserted=0):
-	print('active: ',active, type(active), '\ncondominium: ',condominium, type(condominium), '\ninserted: ', inserted, type(inserted))
+def update_condominium_houses(house, condominium, active, inserted):
 	active = int(active)
 	inserted = int(inserted)
-	print('active: ',active, type(active), '\ncondominium: ',condominium, type(condominium), '\ninserted: ', inserted, type(inserted))
 	condominium_doc = frappe.get_doc('Condominium', condominium)
-	if inserted:
+
+	if not inserted:
+		# actualiza el campo inserted en el documento de housing
+		house_doc = frappe.get_doc('Housing', house)
+		house_doc.inserted = 1
+		house_doc.save()
 		condominium_doc.n_houses += 1
 		if active:
 			condominium_doc.n_houses_active += 1
+		
 	else:
 		if active:
 			condominium_doc.n_houses_active += 1
 		else:
 			condominium_doc.n_houses_active -= 1
 	condominium_doc.save()
+"""
